c92e118ddac21b06d30d8282e4f64afb2e942ac8
raymon-tian <dongtian@nuaa.edu.cn>
2017-06-09 17:14:01 +0800

    增加classifier

---
+++ classifier.py
@@ -0,0 +1,67 @@
+#coding=utf-8
+import pandas as pd
+import shutil
+import numpy as np
+import os
+import glob
+from pprint import pprint
+
+if __name__ == '__main__':
+
+    phase = 'train'
+
+    result_path0 = '/home/lili/opcode-2017-05/train/0'
+    result_path1 = '/home/lili/opcode-2017-05/train/1'
+    result_path2 = '/home/lili/opcode-2017-05/test/0'
+    result_path3 = '/home/lili/opcode-2017-05/test/1'
+
+    result_paths = [result_path0,result_path1,result_path2,result_path3]
+
+    for r in result_paths:
+        if(os.path.exists(r) == False):
+            os.makedirs(r)
+
+    train_csv = '2017-05_train.csv'
+    test_csv = '2017-05_test.csv'
+
+    train_df = pd.read_csv(train_csv)
+    test_df = pd.read_csv(test_csv)
+
+    train_np = train_df.as_matrix()
+    test_np = test_df.as_matrix()
+
+    now_path = os.path.abspath('.')
+    subfolders = os.listdir('.')
+    search_paths = []
+    for p in subfolders:
+        temp = os.path.join(now_path, p)
+        if(os.path.isdir(temp)):
+            search_paths.append(temp)
+    files =  []
+    for p in search_paths:
+        files_per_dir = glob.glob(p+'/*')
+        files.extend(files_per_dir)
+    files = np.array(files)
+    files = files[:,np.newaxis]
+    files = np.concatenate((files,files),axis=1)
+    for i in range(len(files)):
+        files[i,1] = files[i,1].split('/')[-1]
+
+    if(phase == 'train'):
+        refer = train_np
+    else:
+        refer = test_np
+
+    for i in range(len(refer)):
+        item = refer[i]
+        file_name = item[0]+'.opcode'
+        flag = bool(item[1])
+        cases = files[files[:,1] == file_name]
+        if(len(cases)!=0):
+            case_file = cases[0]
+            re_path = ''
+            if(phase=='train'):
+                re_path = result_path0 if flag == 0  else result_path1
+            else:
+                re_path = result_path2 if flag == 0  else result_path3
+            shutil.copy(case_file[0],re_path)
